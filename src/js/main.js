58c0385cf8016d2b8ba790900a66debc9bbfd3e3
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2025-05-18 20:03:22 -0300

    feat: add search

--- src/js/main.js
+++ src/js/main.js
@@ -5,38 +5,54 @@ async function carregaDadosProdutos() {
   return produtosLista;
 }
 
-async function renderizarProdutos(produtos) {
+async function renderizarProdutos(_produtos) {
   const container = document.getElementById("teste");
   container.innerHTML = "";
   
-  if(produtos.length == 0) {
+  if(_produtos.length == 0) {
     container.innerHTML = "<p>Nenhum produto encontrado :( </p>";
     return;
   }
     
-  for (let produto of produtos) {
+  for (let produto of _produtos) {
     let card = await criaElementoProduto(produto);    
     container.appendChild(card);
   }
 }
 
-async function criaElementoProduto(produto) {
+async function criaElementoProduto(_produto) {
   const card = document.createElement("div");
   card.classList.add("card");
 
   card.innerHTML = `
-    <img src="${produto.imagem ? produto.imagem : './src/img/loading.gif'}" alt="${produto.nome}" />
-    <b>${produto.nome}</b>
-    <p>R$ ${produto.preco.toFixed(2)}</p>
+    <img src="${_produto.imagem ? _produto.imagem : './src/img/loading.gif'}" alt="${_produto.nome}" />
+    <b>${_produto.nome}</b>
+    <p>R$ ${_produto.preco.toFixed(2)}</p>
     <button>Comprar</button>
   `;
 
   return card;
 }
 
+function buscaProduto(_produtos){
+   const input = document.getElementById("searchInput");
+   
+   input.addEventListener("input", (event) => {
+    const campoBusca = event.target.value.toLowerCase();
+
+    const filtrados = _produtos.filter((produto) =>
+      produto.nome.toLowerCase().includes(campoBusca)
+    );
+
+    renderizarProdutos(filtrados);
+  });
+}
+
 async function carregaProdutos() {
   const produtos = await carregaDadosProdutos();
+
   renderizarProdutos(produtos);
+  buscaProduto(produtos);
   
   return produtos;
 }
